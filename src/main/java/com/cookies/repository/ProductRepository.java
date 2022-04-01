5bf9f5374542ef9a8c09f8a51ebbf4b2439bbc66
nikitanosov1 <nikinosov2002@mail.ru>
2022-04-02 00:05:34 +0400

    Fix 2

--- src/main/java/com/cookies/repository/ProductRepository.java
+++ src/main/java/com/cookies/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.cookies.repository;
 import com.cookies.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product getProductById(Long id);
+    List<Product> findByNameContaining(String productName);
 }
